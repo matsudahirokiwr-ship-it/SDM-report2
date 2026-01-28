3e406884603799b02b00de84885a158d6e280732
Hiroki Matsuda <matsuda.hiroki.wr@tut.jp>
2026-01-28 22:20:12 +0900

    (3)完了

--- calc_mul.py
+++ calc_mul.py
@@ -5,11 +5,12 @@ import re
 def calc(A,B):
         ai=str(A)
         bi=str(B)
-        p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
-                a=float(ai)
-                b=float(bi)
-                if 0<a and a<b and b<1000:
+        # 整数のみを許可する正規表現
+        p = re.compile(r'^[0-9]+$')
+        if p.match(ai) and p.match(bi):
+                a=int(ai)
+                b=int(bi)
+                if (1 <= a <= 999 and 1 <= b <= 999):
                         valid=True
                 else:
                         valid=False
